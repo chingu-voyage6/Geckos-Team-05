9cd68a989b1e516c99500808e6f3ef36992f74d5
ethioabduselam <aabdu2833@gmail.com>
2018-06-29 21:58:07 +0300

    changed the database schema

--- Server/models/test.js
+++ Server/models/test.js
@@ -2,8 +2,13 @@ var mongoose = require("mongoose");
 
 // Test Schema & Model Setup
   var testItemSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+    source: Object,
+    author: String,
+    title: String,
+    description: String,
+    url: String,
+    urlToImage: String,
+    publishedAt: String
   });
 
   module.exports = mongoose.model("TestItem", testItemSchema);
