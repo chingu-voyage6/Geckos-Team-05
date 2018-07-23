1c8dd37fae18c38312a8eee0aa0f36706f3d10ea
ethioabduselam <aabdu2833@gmail.com>
2018-07-23 21:01:30 +0300

    included search using /api/search/{searchquery}

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 var news = require("../models/news");
 
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.API_KEY);
+
 router.use(function(req, res, next) {
 	// TODO: update this to only add in the development mode
  res.header("Access-Control-Allow-Origin", "*");
@@ -16,6 +19,22 @@ router.get("/api/category/:catName", (req, res) => {
   .then((data) => {
     res.send({data: data});
     console.log("Response sent to get latest "+catName);
+  })
+  .catch((err) => {
+    res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+  });
+});
+
+router.get("/api/search/:query", (req, res) => {
+  let query = req.params.query;
+  console.log("Request recieved to get results for " + query);
+  newsapi.v2.topHeadlines({
+    q: query,
+    pageSize: 9
+  }).then(response => {
+    let array = response.articles;
+    res.send({data: array});
+    console.log("Response sent to get results for " + query);
   })
   .catch((err) => {
     res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
