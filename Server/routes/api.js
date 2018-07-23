59305a39fc1d154b3f9474f2fad05a0c4899b592
ethioabduselam <aabdu2833@gmail.com>
2018-07-23 21:02:35 +0300

    sort articles

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 var news = require("../models/news");
-
-
+var dateFormat = require('dateformat');
 const NewsAPI = require('newsapi');
+
 const newsapi = new NewsAPI(process.env.API_KEY);
 const tagList = ["business","entertainment","general","health","science","sports","technology"];
 
@@ -15,13 +15,19 @@ var refresh = function(){
   var count = 0;
   // Delete existing data before seeding
   news.collection.remove({"category":"topheadlines"});
-  response.articles.forEach(function(article) {
-  if(count<9){
-    article["category"] = "topheadlines";
-    var obj = new news(article);
-    obj.save();
-    count++;
-  }
+  var art = response.articles.sort(function (a, b) {
+    return Date.parse(b.publishedAt) - Date.parse(a.publishedAt)
+  });
+  art.forEach(function(article) {
+    if(count<9){
+      if(article.urlToImage){
+        article.category = "topheadlines";
+        article.publishedAt = timeStampToDate(article.publishedAt);
+        var obj = new news(article);
+        obj.save();
+        count++;
+      }
+    }
   });
 })
 .catch((err) => {
@@ -38,10 +44,14 @@ var updateCategory = function(catName) {
     // Delete existing data before seeding
     var count = 0;
     news.collection.remove({"category":catName});
-    response.articles.forEach(function(article) {
+    var art = response.articles.sort(function (a, b) {
+      return Date.parse(b.publishedAt) - Date.parse(a.publishedAt)
+    });
+    art.forEach(function(article) {
       if(count<9){
-        if(article.urlToImage&&article.description){
-          article["category"] = catName;
+        if(article.urlToImage){
+          article.publishedAt = timeStampToDate(article.publishedAt);
+          article.category = catName;
           var obj = new news(article);
           obj.save();
           count++;
@@ -58,6 +68,12 @@ var refreshCategory = function() {
   tagList.forEach(function(category) {
     updateCategory(category);
   });
+}
+
+function timeStampToDate (timeStamp) {
+  let date = new Date (timeStamp);
+  let formatedDate = dateFormat (date, "mmm dd @ HH:MM");
+  return formatedDate;
 }
 
 refresh();
