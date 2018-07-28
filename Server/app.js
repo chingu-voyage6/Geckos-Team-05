986406a1e3c7a76627d4ef21cebdb1fcca5f8d16
niamurrell <niamurrell@users.noreply.github.com>
2018-07-28 16:37:25 +0100

    Change server path for build files

--- Server/app.js
+++ Server/app.js
@@ -18,11 +18,11 @@ var indexRoutes = require("./routes/index");
 var apiRoutes = require("./routes/api");
 
 // Point Node to React build files
-// app.use(express.static("../Client/build"));
-app.use(express.static(path.join(__dirname, '/Client/build')));
+app.use(express.static("../Client/build"));
+// app.use(express.static(path.join(__dirname, '/Client/build')));
 
 app.get('/', (req, res) => {
-  var file = path.join(__dirname+'/Client/build/index.html');
+  var file = "/Client/build/index.html";
   res.sendFile(file);
   console.log(file);
 });
