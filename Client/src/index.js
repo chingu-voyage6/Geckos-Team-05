00db4e7aea788f441df35efa065eb10ffbc3beec
niamurrell <niamurrell@users.noreply.github.com>
2018-08-01 19:56:22 +0100

    Import CSS files

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import registerServiceWorker from './utils/registerServiceWorker';
 import App from "./components/App.js";
 
+import "./style.css";
+import "./day.css";
+import "./night.css";
+
 ReactDOM.render(
 	<App />,
 	document.getElementById("render")
